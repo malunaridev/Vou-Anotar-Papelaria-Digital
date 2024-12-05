9ec7e97bb98e88bd90e8ad71383aff194cf2897f
MalunariDev <malunaridev@gmail.com>
2024-12-04 23:17:32 -0300

    feat: new style for product page

--- index.js
+++ index.js
@@ -79,3 +79,42 @@ if (item.type.includes("planner")) {
   productPrice.textContent = item.price;
 }
 });
+
+
+const productrGrid1 = document.getElementById("sideProductGrid1")
+
+todososprodutos.forEach((item) => {
+  if (item.display.includes("homeNews")) {
+  const productCard = document.createElement("a");
+  const starBackground = document.createElement("div");
+  const productCollection = document.createElement("a");
+  const productThumb = document.createElement("img");
+  const productTitle = document.createElement("div");
+  const productPrice = document.createElement("div");
+
+  productCard.classList.add("productCard");
+  starBackground.classList.add("starBackground");
+  productCard.style.background = item.cardGradient;
+  productCollection.classList.add("collectionItem");
+  productCollection.style.background = item.collectionTagColor;
+  productThumb.classList.add("productPicture");
+  productTitle.classList.add("productTitle");
+  productPrice.classList.add("itemPrice");
+
+  productrGrid1.appendChild(productCard);
+  productCard.appendChild(starBackground);
+  productCard.appendChild(productCollection);
+  productCard.appendChild(productThumb);
+  productCard.appendChild(productTitle);
+  productCard.appendChild(productPrice);
+
+  productCollection.href = item.collectionLink;
+  productCollection.rel = "alternate";
+  productCard.href = item.itemLink;
+  productCard.rel = "alternate";
+  productCollection.textContent = item.collection;
+  productThumb.src = item.picture1;
+  productTitle.textContent = item.title;
+  productPrice.textContent = item.price;
+}
+});
