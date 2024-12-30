a105ea5a5e48e3e16ba53b25e640d0b87b93cc67
MalunariDev <malunaridev@gmail.com>
2024-12-30 11:07:50 -0300

    style: new footer section

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,14 @@
 const footer = document.getElementById("footerComponent");
 
 footer.innerHTML = `
-    
-     <div class="footerDeco"></div>
+    <div class="footerOverlay"></div>
+    <section class="footerCenter">
+    <div class="footerLogo"></div>
+    </section>
      
+    <div class="footerCredits">
+    <span>
+    Layout e Design por <a href="https://www.behance.net/malunaris" rel="external" target="_blank">Malunari Design Gráfico</a> – Código e desenvolvimento por <a href="https://github.com/malunaridev" rel="external" target="_blank">MalunariDev</a></span>
+    </div>
    
 `;
